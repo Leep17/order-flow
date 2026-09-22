1cb47d58a51a8917f42bcae922fb3bc8b9b57707
Max Solovov <m_solovov1@mail.ru>
2026-09-22 23:18:27 +0300

    Initial project structure

---
+++ order-service/src/main/java/ru/practicum/order_flow/controller/PingController.java
@@ -0,0 +1,14 @@
+package ru.practicum.order_flow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+    @GetMapping
+public String ping() {
+        return "order-service";
+    }
+}
